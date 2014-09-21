cfd5a878db8085bf3e523b6167f2462176bdef4a
Brian Downs <brian.downs@gmail.com>
2014-09-21 14:28:42 -0700

    Testing refactor of salt implementation.

--- password_utility.go
+++ password_utility.go
@@ -123,6 +123,7 @@ func (p *Password) MD5(saltConf ...*SaltConf) ([16]byte, []byte) {
 // Generate a SHA256 sum for the given password.  If a SaltConf
 // pointer is given as a parameter a salt with the given
 // length will be returned with it included in the hash.
+/*
 func (p *Password) SHA256(saltConf ...*SaltConf) ([32]byte, []byte) {
 	if len(saltConf) > 0 {
 		var saltLength int
@@ -131,6 +132,19 @@ func (p *Password) SHA256(saltConf ...*SaltConf) ([32]byte, []byte) {
 		}
 		salt := getRandomBytes(saltLength)
 		return sha256.Sum256([]byte(fmt.Sprintf("%s%x", p.Pass, salt))), salt
+	}
+	return sha256.Sum256([]byte(p.Pass)), nil
+}
+*/
+// Generate a SHA256 sum for the given password.  If a SaltConf
+// pointer is given as a parameter a salt with the given
+// length will be returned with it included in the hash.
+func (p *Password) SHA256(saltConf interface{}) ([32]byte, []byte) {
+	if len(saltConf) > 0 {
+		if saltConf {
+			salt := getRandomBytes(saltConf.Length)
+			return sha256.Sum256([]byte(fmt.Sprintf("%s%x", p.Pass, salt))), salt
+		}
 	}
 	return sha256.Sum256([]byte(p.Pass)), nil
 }
